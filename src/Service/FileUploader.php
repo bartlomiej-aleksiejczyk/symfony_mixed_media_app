4dda3d925a6d199442c61ed0ae087d43f05dfe3f
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-11-08 19:36:52 +0100

    feat: add the AnonymousMessage component

---
+++ src/Service/FileUploader.php
@@ -0,0 +1,36 @@
+<?php
+// src/Service/FileUploader.php
+namespace App\Service;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class FileUploader
+{
+    private string $targetDirectory;
+
+    public function __construct(
+        private SluggerInterface $slugger,
+        ParameterBagInterface    $params
+    )
+    {
+        $this->targetDirectory = $params->get('anonymous_attachments_directory');
+    }
+
+    public function upload(UploadedFile $file): string
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $newFilename = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+        try {
+            $file->move($this->targetDirectory, $newFilename);
+        } catch (FileException $e) {
+            // handle error
+        }
+
+        return $newFilename;
+    }
+}

0b293d1e0098e5391a559a6f6a46ad9d3ca8cdda
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-11-13 21:36:09 +0100

    fix: rename user the table and regenerate migrations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
+#[ORM\Table(name: 'app_user')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -33,7 +34,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    public function getId(): ?int
+    public function getId(): ?string
     {
         return $this->id;
     }
